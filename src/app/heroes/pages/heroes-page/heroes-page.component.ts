bd62e71a8becca8d28f933ca08cd5e4b25a0c7d4
Sebastián Aguirre <guiablanco@live.cl>
2024-01-08 15:27:42 -0300

    añadiendo pipes y heroes services

--- src/app/heroes/pages/heroes-page/heroes-page.component.ts
+++ src/app/heroes/pages/heroes-page/heroes-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HeroesService } from './../../services/heroes.service';
+import { Component, OnInit } from '@angular/core';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-heroes-page',
@@ -6,6 +10,29 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class HeroesPageComponent {
+export class HeroesPageComponent implements OnInit {
+
+  public hero?: Hero
+
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+    ){}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({ id}) => this.heroesService.getHeroById(id)  )
+      ).subscribe( hero => {
+        if( !hero ) return this.router.navigate(['/heroes/list']);
+
+        this.hero = hero;
+        console.log({hero});
+
+
+        return;
+      })
+  }
 
 }
